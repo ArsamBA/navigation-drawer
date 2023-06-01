7a0af83b12c7414648f15f3ffdc48a4b657e7a87
Arsam Bagheramiri <arsam5ba5@gmail.com>
2023-06-01 22:53:48 +0330

    assests in necessary files added

---
+++ navigation/layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+   <div>
+      <SideNavigation :navigationData="navigationData" />
+      <slot />
+
+   </div>
+</template>
+
+<script setup lang="ts">
+const navigationData = ref({
+   image_url: 'https://avatars.githubusercontent.com/u/104560377?v=4',
+   image_alt: 'Arsam',
+   name: 'Arsam',
+   navLink: [
+      {
+         path: '/',
+         name: 'Home',
+         icon : '',
+
+      },
+      {
+         path: '/blog',
+         name: 'Blog',
+         icon : '',
+
+      },
+      {
+         path: '/about',
+         name: 'About',
+         icon : '',
+
+      },
+      {
+         path: '/contact',
+         name: 'Contact',
+         icon : '',
+
+      }
+   ],
+})
+</script>
+
+<style>
+
+</style>
---
+++ navigation/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/global.scss';
+
+</style>
